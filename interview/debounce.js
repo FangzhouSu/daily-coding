8dc9a590f2bb2c440be5a04dd9c0b168d3f358b8
FangzhouSu <1773279395@qq.com>
2022-07-17 21:22:30 +0800

    feat: 优化防抖函数用例

--- interview/debounce.js
+++ interview/debounce.js
@@ -1,4 +1,4 @@
-function debounce(fn, delay = 1000) {
+function debounce(fn, delay = 2000) {
   let timer;
   return function (...args) {
     if (timer) {
@@ -15,4 +15,15 @@ const testFunc = (...args) => {
   console.log('the args of the testFunc:', args);
 }
 
-debounce(testFunc)('test debouce', 'test2');
+const test = debounce(testFunc);
+
+// 模拟每半秒会触发一次事件，但是使用防抖函数包了一层之后，只有[最后一次触发结束后，隔delay的时间]才会真正触发，避免触发次数过多带来的性能损耗
+let time = 0;
+const timer = setInterval(() => {
+  test('test debouce', 'test2');
+  time++;
+  console.log('flag每增加一下，说明触发了事件，不过因为有防抖函数，time为6 & 过了delay的时间后会开启定时器', time);
+  if (time === 6) {
+    clearTimeout(timer);
+  }
+}, 500);
