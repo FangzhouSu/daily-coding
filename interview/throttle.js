c0089ca526fb9584d1b01eadd298e7a3803cad0d
FangzhouSu <1773279395@qq.com>
2022-07-17 21:24:56 +0800

    style: 改一下注释

--- interview/throttle.js
+++ interview/throttle.js
@@ -19,7 +19,7 @@ const testFunc = (...args) => {
 
 const test = throttle(testFunc);
 
-// 模拟每半秒会触发一次事件，但是使用节流函数包了一层之后，只有隔delay的时间才会真正触发，避免触发次数过多带来的性能损耗
+// 模拟每半秒会触发一次事件，但是使用节流函数包了一层之后，只有[隔delay的时间]才会真正触发此事件，避免触发次数过多带来的性能损耗
 setInterval(() => {
   test('test throttle', 'test2');
 }, 500)
