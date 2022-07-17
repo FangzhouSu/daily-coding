220bd4a2c39dc6b4c79ebaee32cc452e12d6351b
FangzhouSu <1773279395@qq.com>
2022-07-17 21:11:39 +0800

    feat: 优化节流用例

--- interview/throttle.js
+++ interview/throttle.js
@@ -1,10 +1,11 @@
 // 向林三心学习的比较简短的节流函数 不需要对时间进行计算
-function throttle(fn, delay = 500) {
+function throttle(fn, delay = 2000) {
   let flag = true;
   return function (...args) {
+    console.log('flag为false时不会启动一个新的定时器，直接return掉～', flag);
     if (!flag) return;
     flag = false; // 实现节流的关键flag——除非fn执行完成，否则不管点多少次，flag都是false
-    console.log('the arguments are:', arguments);
+    // console.log('the arguments are:', arguments); // arguments是个对象： { '0': 'test throttle', '1': 'test2' }
     setTimeout(() => {
       fn.apply(this, args);
       flag = true;
@@ -16,4 +17,9 @@ const testFunc = (...args) => {
   console.log('the args of the testFunc:', args);
 }
 
-throttle(testFunc)('test throttle', 'test2');
+const test = throttle(testFunc);
+
+setInterval(() => {
+  test('test throttle', 'test2');
+}, 500)
+// throttle(testFunc)('test throttle', 'test2');
